9cdb253a996a7a8149f7a79f020a9ad5405a26ba
Donna <skies09@hotmail.com>
2019-09-17 11:56:05 +0100

    adding deliveries page

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from .views import home
+from .views import deliveries
 from .views import contact
 
 
 urlpatterns = [
 url(r'^home/', home, name="home"),
+url(r'^deliveries/', deliveries, name="deliveries"),
 url(r'^contact/', contact, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,6 +5,9 @@ def home(request):
     """A view that displays the home page"""
     return render(request, "home.html")
 
+def deliveries(request):
+    """A view that displays the contact page"""
+    return render(request, "deliveries.html")
 
 def contact(request):
     """A view that displays the contact page"""
