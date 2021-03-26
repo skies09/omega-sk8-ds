3711426afbff4db739d782505902351ee254dd27
Donna <skies09@hotmail.com>
2021-03-26 16:38:45 +0000

    update wsgi

--- sk8/wsgi.py
+++ sk8/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sk8.settings")
 
