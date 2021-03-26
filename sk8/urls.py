504a80df2b248d4d5d4ff2c7f245797c3b3ea4d3
Donna <skies09@hotmail.com>
2021-03-26 16:07:50 +0000

    fix media root

--- sk8/urls.py
+++ sk8/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import home
@@ -37,4 +39,4 @@ urlpatterns = [
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^blog/', include(urls_blog)),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
